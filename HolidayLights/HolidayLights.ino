ac08b96c9e3e1da9ff656bef3352e3dd2c4cb4b8
CrimsonOrca <b.miah0987@gmail.com>
2023-01-14 13:45:06 -0500

    refactored code and reduced program memory storage...

--- HolidayLights/HolidayLights.ino
+++ HolidayLights/HolidayLights.ino
@@ -1,86 +1,54 @@
+#include "Utils.h"
+
 int readValue;
 int brightness;
 
-constexpr int RED = 11;
-constexpr int GREEN = 10;
-constexpr int BLUE = 9;
-constexpr int YELLOW = 6;
-constexpr int BUTTON = 5;
-constexpr int NUMBER_BLINKS = 10;
-constexpr int DIM = 0;
-
-bool buttonPressed = false;
-
 void setup() {
 
-  constexpr int pins[] {YELLOW, BLUE, GREEN, RED};
-  for (const auto& pin: pins)
-    pinMode(pin, OUTPUT);
-
+  for (auto start = LEDs.begin(); start <= LEDs.end(); start++)
+    pinMode(*start, OUTPUT);
+    
   pinMode(BUTTON, INPUT); 
+
 }
 
 void loop() {
+
   readValue = analogRead(A0);
   brightness = readValue / 4;
 
-  buttonPressed = (digitalRead(BUTTON) == HIGH);
+  switchLights(Mode::ON, brightness);
 
-  analogWrite(YELLOW, brightness);
-  analogWrite(BLUE, brightness);
-  analogWrite(GREEN, brightness);
-  analogWrite(RED, brightness);
-
-  if (buttonPressed) {
+  if (digitalRead(BUTTON) == HIGH) {
     
-    analogWrite(RED, DIM);
-    analogWrite(GREEN, DIM);
-    analogWrite(BLUE, DIM);
-    analogWrite(YELLOW, DIM);
-    delay(1000);
+    switchLights(Mode::OFF);
+    delay(ONE_SECOND);
 
-    // light show...
-    analogWrite(RED, brightness);
-    delay(250);
-    analogWrite(GREEN, brightness);
-    delay(250);
-    analogWrite(BLUE, brightness);
-    delay(250);
-    analogWrite(YELLOW, brightness);
-    delay(250);
-    analogWrite(RED, DIM);
-    analogWrite(GREEN, DIM);
-    analogWrite(BLUE, DIM);
-    analogWrite(YELLOW, DIM);
-    delay(1000);
+    // begin light show...
+    for (auto start = LEDs.begin(); start <= LEDs.end(); start++) {
+        analogWrite(*start, brightness);
+        delay(QUARTER_SECOND);
+    }
+    switchLights(Mode::OFF);
+    delay(ONE_SECOND);
+ 
+    for (auto start = LEDs.end(); start >= LEDs.begin(); start--) {
+      analogWrite(*start, brightness);
+      delay(QUARTER_SECOND);
+    }
+    switchLights(Mode::OFF);
+    delay(ONE_SECOND);
 
-    analogWrite(YELLOW, brightness);
-    delay(250);
-    analogWrite(BLUE, brightness);
-    delay(250);
-    analogWrite(GREEN, brightness);
-    delay(250);
-    analogWrite(RED, brightness);
-    delay(250);
-    analogWrite(RED, DIM);
-    analogWrite(GREEN, DIM);
-    analogWrite(BLUE, DIM);
-    analogWrite(YELLOW, DIM);
-    delay(1000);
-
-    for (int i = 0; i < NUMBER_BLINKS; i++) {
-      analogWrite(RED, brightness);
-      analogWrite(GREEN, brightness);
-      analogWrite(BLUE, brightness);
-      analogWrite(YELLOW, brightness);
-      delay(250);
-      analogWrite(RED, DIM);
-      analogWrite(GREEN, DIM);
-      analogWrite(BLUE, DIM);
-      analogWrite(YELLOW, DIM);
-      delay(250);
+    for (int n = 0; n < NUMBER_BLINKS; n++) {
+      switchLights(Mode::ON, brightness);
+      delay(QUARTER_SECOND);
+      switchLights(Mode::OFF);
+      delay(QUARTER_SECOND);
     }
   }
+
 }
+
+
 
 
